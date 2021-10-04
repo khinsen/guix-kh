acfa231a7423a770194fb7eb2bbb92f3e1292f94
Konrad Hinsen <konrad.hinsen@fastmail.net>
2021-10-04 11:43:48 +0200

    Draft package for font-julia-mono

--- kh/packages.scm
+++ kh/packages.scm
@@ -14,7 +14,9 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system python)
+  #:use-module (guix build-system font)
   #:use-module (gnu packages astronomy)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages compression))
@@ -72,3 +74,29 @@
      "A full featured python library to read from and write to FITS files.")
     (license license:gpl2)))
 
+(define-public font-julia-mono
+  (package
+    (name "font-julia-mono")
+    (version "0.043")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/cormullion/juliamono/releases/download/"
+                    "v" version "/JuliaMono-ttf.tar.gz"))
+              (sha256
+               (base32
+                "0vb7n9yqgasnxzps13ckklay5bla6b0i79pzmfqvjms1r37079gh"))))
+    (build-system font-build-system)
+    (arguments
+     `(#:phases (modify-phases %standard-phases
+                  (replace 'unpack
+                    (lambda* (#:key source #:allow-other-keys)
+                      (mkdir "source")
+                      (chdir "source")
+                      (invoke "tar" "xzf" source))))))
+    (native-inputs `(("tar" ,tar)))
+    (home-page "https://juliamono.netlify.app/")
+    (synopsis "JuliaMono typeface")
+    (description "The JuliaMono font is a monospaced font with good Unicode
+coverage, in particular for the needs of scientific and technical computing.")
+    (license license:silofl1.1)))
