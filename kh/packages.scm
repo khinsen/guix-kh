95bb436980ae0a304efaaa3fcf7108df79f9f48b
Konrad Hinsen <konrad.hinsen@fastmail.net>
2021-10-12 10:24:08 +0200

    Remove python-fitsio (now in Guix)

--- kh/packages.scm
+++ kh/packages.scm
@@ -21,62 +21,9 @@
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages compression))
 
-(define-public python-fitsio
+(define-public font-juliamono
   (package
-    (name "python-fitsio")
-    (version "1.1.5")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "fitsio" version))
-       (sha256
-        (base32 "1llql2i6xr9lkdl81jx5nvz80kspamvira90546y32ldy551hq1l"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'unbundle-cfitsio
-           (lambda* (#:key inputs #:allow-other-keys)
-             (let* ((cfitsio (assoc-ref inputs "cfitsio"))
-                    (includedir (string-append "\"" cfitsio "/include\""))
-                    (libdir (string-append "\"" cfitsio "/lib\"")))
-               (substitute* "setup.py"
-                 (("self.use_system_fitsio = False") "pass")
-                 (("self.system_fitsio_includedir = None") "pass")
-                 (("self.system_fitsio_libdir = None") "pass")
-                 (("self.use_system_fitsio") "True")
-                 (("self.system_fitsio_includedir") includedir)
-                 (("self.system_fitsio_libdir") libdir))
-               )
-             ;; cfitsio is built without bzip2 support, so the bzip2 test
-             ;; must fail.
-             (substitute* "fitsio/test.py"
-               (("'SKIP_BZIP_TEST' in os.environ") "True"))
-             ;; (let* ((bzip2 (assoc-ref inputs "bzip2"))
-             ;;        (bzip2-binary (string-append bzip2 "bin/bzip2")))
-             ;;   (substitute* "fitsio/test.py"
-             ;;     (("os.system('bzip2 %s' % fname)")
-             ;;      (string-append
-             ;;       "os.system('"
-             ;;       bzip2-binary
-             ;;       " %s' % fname)"))))
-             #t))
-         )))
-    (propagated-inputs
-     `(("python-numpy" ,python-numpy)
-       ("cfitsio" ,cfitsio)))
-    (native-inputs
-     `(("bzip2" ,bzip2))) ;; used in tests
-    (home-page "https://github.com/esheldon/fitsio")
-    (synopsis
-     "Python library to read from and write to FITS files")
-    (description
-     "A full featured python library to read from and write to FITS files.")
-    (license license:gpl2)))
-
-(define-public font-julia-mono
-  (package
-    (name "font-julia-mono")
+    (name "font-juliamono")
     (version "0.043")
     (source (origin
               (method url-fetch)
