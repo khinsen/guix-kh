11e1d845ca054c50ca3df1564ee2ac85493acef3
Konrad Hinsen <konrad.hinsen@fastmail.net>
2021-10-27 20:22:03 +0200

    Skeleton for ocrmypdf

--- kh/packages.scm
+++ kh/packages.scm
@@ -18,7 +18,11 @@
   #:use-module (gnu packages check)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-check)
-  #:use-module (gnu packages python-xyz))
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages libffi)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages time)
+  )
 
 (define-public python-pytest-subtests
   (package
@@ -61,3 +65,78 @@
    (synopsis "Physical quantities module")
    (description "Physical quantities module")
    (license license:bsd-3)))
+
+(define-public python-pymupdf
+  (package
+    (name "python-pymupdf")
+    (version "1.19.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "PyMuPDF" version))
+       (sha256
+        (base32 "0xgx4szwmdjxkq9c9zwbal8k14gc5jz9j1mnjmxk57sjs5qwcw2a"))))
+    (build-system python-build-system)
+    (inputs
+     `(("mupdf" ,mupdf)))
+    (propagated-inputs
+     `(("python-pillow" ,python-pillow)
+       ("python-fonttools" ,python-fonttools)))
+    (home-page "https://github.com/pymupdf/PyMuPDF")
+    (synopsis "Python bindings for the PDF toolkit and renderer MuPDF")
+    (description "Python bindings for the PDF toolkit and renderer MuPDF")
+    (license #f)))
+
+(define-public python-xmp-toolkit
+  (package
+    (name "python-xmp-toolkit")
+    (version "2.0.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "python-xmp-toolkit" version))
+       (sha256
+        (base32 "0f4s86hji6idyfg9007jncl366gasjjmldbwbknldzgrdya15ngq"))))
+    (build-system python-build-system)
+    (propagated-inputs `(("python-pytz" ,python-pytz)))
+    (home-page "https://github.com/python-xmp-toolkit/python-xmp-toolkit")
+    (synopsis "Python XMP Toolkit for working with metadata.")
+    (description "Python XMP Toolkit for working with metadata.")
+    (license #f)))
+
+(define-public ocrmypdf
+  (package
+    (name "ocrmypdf")
+    (version "12.7.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "ocrmypdf" version))
+       (sha256
+        (base32 "136nq1awnld6bb26268dggay7fjwbxrsk54ky136mnlvllgd8dsv"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-cffi" ,python-cffi)
+       ("python-coloredlogs" ,python-coloredlogs)
+       ("img2pdf" ,img2pdf)
+       ("python-importlib-metadata" ,python-importlib-metadata)
+       ("python-importlib-resources" ,python-importlib-resources)
+       ("python-pdfminer-six" ,python-pdfminer-six)
+       ("python-pikepdf" ,python-pikepdf)
+       ("python-pillow" ,python-pillow)
+       ("python-pluggy" ,python-pluggy)
+       ("python-reportlab" ,python-reportlab)
+       ("python-tqdm" ,python-tqdm)))
+    (native-inputs
+     `(("python-coverage" ,python-coverage)
+       ("python-pymupdf" ,python-pymupdf)
+       ("python-pytest" ,python-pytest)
+       ("python-pytest-cov" ,python-pytest-cov)
+       ("python-pytest-xdist" ,python-pytest-xdist)
+       ("python-xmp-toolkit" ,python-xmp-toolkit)))
+    (home-page "https://github.com/jbarlow83/OCRmyPDF")
+    (synopsis
+     "OCRmyPDF adds an OCR text layer to scanned PDF files, allowing them to be searched")
+    (description
+     "OCRmyPDF adds an OCR text layer to scanned PDF files, allowing them to be searched")
+    (license #f)))
