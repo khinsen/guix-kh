14d875c0b13e9fdee0cfc0569dcaaf7187f99c38
Konrad Hinsen <konrad.hinsen@fastmail.net>
2021-10-27 18:57:03 +0200

    Remove font-juliamono (now in Guix), add draft for python-pint

--- kh/packages.scm
+++ kh/packages.scm
@@ -14,36 +14,50 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system python)
-  #:use-module (guix build-system font)
-  #:use-module (gnu packages astronomy)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages python)
-  #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages compression))
+  #:use-module (gnu packages python-check)
+  #:use-module (gnu packages python-xyz))
 
-(define-public font-juliamono
+(define-public python-pytest-subtests
   (package
-    (name "font-juliamono")
-    (version "0.043")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "https://github.com/cormullion/juliamono/releases/download/"
-                    "v" version "/JuliaMono-ttf.tar.gz"))
-              (sha256
-               (base32
-                "0vb7n9yqgasnxzps13ckklay5bla6b0i79pzmfqvjms1r37079gh"))))
-    (build-system font-build-system)
-    (arguments
-     `(#:phases (modify-phases %standard-phases
-                  (replace 'unpack
-                    (lambda* (#:key source #:allow-other-keys)
-                      (mkdir "source")
-                      (chdir "source")
-                      (invoke "tar" "xzf" source))))))
-    (native-inputs `(("tar" ,tar)))
-    (home-page "https://juliamono.netlify.app/")
-    (synopsis "JuliaMono typeface")
-    (description "The JuliaMono font is a monospaced font with good Unicode
-coverage, in particular for the needs of scientific and technical computing.")
-    (license license:silofl1.1)))
+    (name "python-pytest-subtests")
+    (version "0.5.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "pytest-subtests" version))
+       (sha256
+        (base32 "087i03nmkmfnrpc7mmizvr40ijnjw4lfxc22rnk8jk6s1szy9lav"))))
+    (build-system python-build-system)
+    (propagated-inputs `(("python-pytest" ,python-pytest)))
+    (home-page "https://github.com/pytest-dev/pytest-subtests")
+    (synopsis "unittest subTest() support and subtests fixture")
+    (description "unittest subTest() support and subtests fixture")
+    (license license:expat)))
+
+(define-public python-pint
+  (package
+   (name "python-pint")
+   (version "0.17")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "Pint" version))
+     (sha256
+      (base32 "04zklf8ba7zj6garfr753x2nd19m4wjgxvn6lx3ni7i32fkwml7l"))))
+   (build-system python-build-system)
+   (propagated-inputs
+    `(("python-importlib-metadata" ,python-importlib-metadata)
+      ("python-importlib-resources" ,python-importlib-resources)
+      ("python-packaging" ,python-packaging)))
+   (native-inputs
+    `(("python-pytest" ,python-pytest)
+      ("python-pytest-cov" ,python-pytest-cov)
+      ("python-pytest-mpl" ,python-pytest-mpl)
+      ("python-pytest-subtests" ,python-pytest-subtests)))
+   (home-page "https://github.com/hgrecco/pint")
+   (synopsis "Physical quantities module")
+   (description "Physical quantities module")
+   (license license:bsd-3)))
