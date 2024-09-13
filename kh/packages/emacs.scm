8f61a2d3e7f60e6e95fd0f87f9ee56a12cf04cee
Konrad Hinsen <konrad.hinsen@fastmail.net>
2024-09-13 12:09:38 +0200

    Cleanup

--- kh/packages/emacs.scm
+++ kh/packages/emacs.scm
@@ -1,85 +1,13 @@
 (define-module (kh packages emacs)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages texinfo)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix gexp)
   #:use-module (guix build-system emacs))
-
-(define-public emacs-org-cliplink
-  (package
-   (name "emacs-org-cliplink")
-   (version "20190608.2134")
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/rexim/org-cliplink")
-           (commit "82402cae7e118d67de7328417fd018a18f95fac2")))
-     (file-name (git-file-name name version))
-     (sha256
-      (base32
-       "0cx0bmpsniazpl420cck9pgvmyfznrvl5bgrjnfyvkmqgpz9nyzv"))))
-   (build-system emacs-build-system)
-   (home-page
-    "http://github.com/rexim/org-cliplink")
-   (synopsis
-    "insert org-mode links from the clipboard")
-   (description
-    "A simple command that takes a URL from the clipboard and inserts an
-org-mode link with a title of a page found by the URL into the
-current buffer
-
-This code was a part of my Emacs config almost a year. I decided to
-publish it as a separate package in case someone needs this feature
-too.
-")
-   (license (license:non-copyleft #:uri "http://github.com/rexim/org-cliplink/org-cliplink.el
-"))))
-
-(define-public emacs-pinboard
-  (package
-   (name "emacs-pinboard")
-   (version "1.2")
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/davep/pinboard.el")
-           (commit (string-append "v" version))))
-     (file-name (git-file-name name version))
-     (sha256
-      (base32
-       "0ljxvp9yppnmy1lnnipqqbk09bmay523haa8h0gnfnrl02jwgs91"))))
-   (build-system emacs-build-system)
-   (home-page
-    "https://github.com/davep/pinboard.el")
-   (synopsis "A pinboard.in client")
-   (description
-    "pinboard.el provides an Emacs client for pinboard.in.
-
-To get started, visit your password settings page
-(https://pinboard.in/settings/password) on Pinboard and get the API token
-that's displayed there. Then edit ~/.authinfo and add a line like this:
-
-machine api.pinboard.in password foo:8ar8a5w188l3
-
-Once done, you can M-x pinboard RET and browse your pins. A number of
-commands are available when viewing the pin list, press \"?\" or see the
-\"Pinboard\" menu for more information.
-
-Commands available that aren't part of the pin list, and that you might
-want to bind to keys, include:
-
-| Command                | Description                           |
-| ---------------------- | ------------------------------------- |
-| pinboard               | Open the Pinboard pin list            |
-| pinboard-add           | Add a new pin to Pinboard             |
-| pinboard-add-for-later | Prompt for a URL and add it for later |
-")
-   (license license:gpl3)))
 
 (define-public emacs-guess-language
   (package
@@ -131,77 +59,3 @@ trigrams.")
    (description
     "Add support for org links to PDF buffers.")
    (license license:gpl3+)))
-
-;; (define-public emacs-org-roam-ui
-;;   (package
-;;     (name "emacs-org-roam-ui")
-;;     (version "20211209.1418")
-;;     (source
-;;      (origin
-;;        (method git-fetch)
-;;        (uri (git-reference
-;;              (url "https://github.com/org-roam/org-roam-ui.git")
-;;              (commit "9ed0c5705a302a91fab2b8bcc777a12dcf9b3682")))
-;;        (sha256
-;;         (base32 "1am11vnzklv0cbivsw5r8x8fx457166mvfgyv7cdhrz88s8iqm23"))))
-;;     (build-system emacs-build-system)
-;;     (propagated-inputs
-;;      (list emacs-org-roam emacs-simple-httpd emacs-websocket))
-;;     (arguments
-;;      '(#:include
-;;        '("^[^/]+.el$"
-;;          "^[^/]+.el.in$"
-;;          "^dir$"
-;;          "^[^/]+.info$"
-;;          "^[^/]+.texi$"
-;;          "^[^/]+.texinfo$"
-;;          "^doc/dir$"
-;;          "^doc/[^/]+.info$"
-;;          "^doc/[^/]+.texi$"
-;;          "^doc/[^/]+.texinfo$"
-;;          "^out$")
-;;        #:exclude
-;;        '("^.dir-locals.el$"
-;;          "^test.el$"
-;;          "^tests.el$"
-;;          "^[^/]+-test.el$"
-;;          "^[^/]+-tests.el$")))
-;;     (home-page "https://github.com/org-roam/org-roam-ui")
-;;     (synopsis "Browser-based user interface for Org-roam")
-;;     (description
-;;      "Org-roam-ui provides a web interface for navigating around notes created
-;; within Org-roam.")
-;;     (license license:gpl3+)))
-
-(define-public emacs-mastodon-unstable
-  (package
-    (name "emacs-mastodon-unstable")
-    (version "3b35b51a36976d64bc3368d6bd8cce5edb350839")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://codeberg.org/martianh/mastodon.el.git")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1d39y7g8ffbw9fgnlqqxm3w2gijmzaigf9qni59f84yx3iw8gpqa"))))
-    (build-system emacs-build-system)
-    (arguments
-     (list #:phases
-           #~(modify-phases %standard-phases
-               ;; Move the source files to the top level, which is included in
-               ;; the EMACSLOADPATH.
-               (add-after 'unpack 'move-source-files
-                 (lambda _
-                   (let ((el-files (find-files "./lisp" ".*\\.el$")))
-                     (for-each (lambda (f)
-                                 (rename-file f (basename f)))
-                               el-files)))))))
-    (propagated-inputs
-     (list emacs-request))
-    (home-page "https://codeberg.org/martianh/mastodon.el")
-    (synopsis "Emacs client for Mastodon")
-    (description "@code{mastodon.el} is an Emacs client for Mastodon, the
-federated microblogging social network.")
-    (license license:gpl3+)))
